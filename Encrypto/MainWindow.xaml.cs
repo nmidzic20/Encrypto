391ad3ce39a21700530f9f68186bba70e75a3ac8
Noa Midzic <nmidzic@yahoo.com>
2023-10-18 17:36:27 +0200

    Add project files.

---
+++ Encrypto/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Encrypto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Upload_Files_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Encrypt_Symmetric_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decrypt_Symmetric_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Encrypt_Asymmetric_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decrypt_Asymmetric_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Calculate_Hash_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Make_Digital_Signature_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Check_Digital_Signature_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Generate_Keys_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
