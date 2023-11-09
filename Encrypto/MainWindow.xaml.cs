3453f7873d3617c348b22fafc9aca9a295ac46fe
Noa Midzic <nmidzic@yahoo.com>
2023-11-09 18:35:55 +0100

    Tested AES and RSA key generation using System.Security.Cryptography and saving them to desktop files

--- Encrypto/MainWindow.xaml.cs
+++ Encrypto/MainWindow.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace Encrypto
 {
@@ -23,6 +24,60 @@ namespace Encrypto
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Generate_Keys_Click(object sender, RoutedEventArgs e)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string secretKeyPath = Path.Combine(desktopPath, "tajni_kljuc.txt");
+            string publicKeyPath = Path.Combine(desktopPath, "javni_kljuc.txt");
+            string privateKeyPath = Path.Combine(desktopPath, "privatni_kljuc.txt");
+
+            GenerateAndWriteAesKey(secretKeyPath);
+            GenerateAndWriteRSAKeys(publicKeyPath, privateKeyPath);
+
+
+        }
+
+        public void GenerateAndWriteAesKey(string filePath)
+        {
+            try
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.GenerateKey();
+
+                    string keyString = Convert.ToBase64String(aesAlg.Key);
+
+                    File.WriteAllText(filePath, keyString);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        public static void GenerateAndWriteRSAKeys(string publicKeyPath, string privateKeyPath)
+        {
+            try
+            {
+                using (RSA rsaAlg = RSA.Create())
+                {
+                    string publicKey = Convert.ToBase64String(rsaAlg.ExportRSAPublicKey());
+
+                    string privateKey = Convert.ToBase64String(rsaAlg.ExportRSAPrivateKey());
+
+                    File.WriteAllText(publicKeyPath, publicKey);
+                    File.WriteAllText(privateKeyPath, privateKey);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
 
         private void Upload_Files_Click(object sender, RoutedEventArgs e)
@@ -65,9 +120,5 @@ namespace Encrypto
 
         }
 
-        private void Generate_Keys_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
