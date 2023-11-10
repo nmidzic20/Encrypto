eb61b101c755ab0375448190b71ed4473ea4a6e8
Noa Midzic <nmidzic@yahoo.com>
2023-11-10 10:03:52 +0100

    AES encryption and decryption implemented and tested, a bug with the start of the text remaining encrypted after decryption

--- Encrypto/MainWindow.xaml.cs
+++ Encrypto/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Security.Cryptography;
 using System.IO;
+using Microsoft.Win32;
 
 namespace Encrypto
 {
@@ -37,6 +38,7 @@ namespace Encrypto
             GenerateAndWriteAesKey(secretKeyPath);
             GenerateAndWriteRSAKeys(publicKeyPath, privateKeyPath);
 
+            MessageBox.Show("AES and RSA keys successfully generated");
 
         }
 
@@ -87,12 +89,120 @@ namespace Encrypto
 
         private void Encrypt_Symmetric_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Title = "Select a file to encrypt";
+                openFileDialog.Filter = "Text Files (*.txt)|*.txt";
 
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    string filePath = openFileDialog.FileName;
+                    EncryptFileWithAes(filePath);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public static void EncryptFileWithAes(string filePath)
+        {
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string keyFilePath = Path.Combine(desktopPath, "tajni_kljuc.txt");
+
+                if (File.Exists(keyFilePath))
+                {
+                    string keyString = File.ReadAllText(keyFilePath);
+                    byte[] key = Convert.FromBase64String(keyString);
+
+                    using (Aes aesAlg = Aes.Create())
+                    {
+                        aesAlg.Key = key;
+
+                        byte[] fileBytes = File.ReadAllBytes(filePath);
+
+                        byte[] encryptedBytes = aesAlg.CreateEncryptor().TransformFinalBlock(fileBytes, 0, fileBytes.Length);
+
+                        string encryptedFilePath = Path.Combine(desktopPath, "encryptedFileAES.txt");
+                        File.WriteAllBytes(encryptedFilePath, encryptedBytes);
+
+                        MessageBox.Show($"File encrypted and saved to {encryptedFilePath} successfully.");
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Please generate keys first before encrypting a file.", "Key Not Found", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void DecryptFileWithAes(string filePath)
+        {
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string keyFilePath = Path.Combine(desktopPath, "tajni_kljuc.txt");
+
+                if (File.Exists(keyFilePath))
+                {
+                    string keyString = File.ReadAllText(keyFilePath);
+                    byte[] key = Convert.FromBase64String(keyString);
+
+                    using (Aes aesAlg = Aes.Create())
+                    {
+                        aesAlg.Key = key;
+
+                        byte[] encryptedBytes = File.ReadAllBytes(filePath);
+
+                        byte[] decryptedBytes = aesAlg.CreateDecryptor().TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+
+                        string decryptedFilePath = Path.Combine(desktopPath, "decryptedFileAES.txt");
+                        File.WriteAllBytes(decryptedFilePath, decryptedBytes);
+
+                        MessageBox.Show($"File decrypted and saved to {decryptedFilePath} successfully.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("AES key file not found on the desktop. The file cannot be decrypted without the key with which it was encrypted.", "Key Not Found", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void Decrypt_Symmetric_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Title = "Select a file to decrypt";
+                openFileDialog.Filter = "Text Files (*.txt)|*.txt";
 
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    string filePath = openFileDialog.FileName;
+                    DecryptFileWithAes(filePath);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void Encrypt_Asymmetric_Click(object sender, RoutedEventArgs e)
